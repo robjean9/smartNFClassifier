f40ad113ddcfc3652bbcf2e12fbb389e52c135de
luizgununes <lgnunes.pessoal@gmail.com>
2023-08-03 14:46:59 -0300

    front version 1

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,9 +1,41 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import {
+  NgxScannerQrcodeComponent,
+  ScannerQRCodeConfig,
+  ScannerQRCodeResult,
+} from 'ngx-scanner-qrcode';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
+  @ViewChild('action') action!: NgxScannerQrcodeComponent;
+
   title = 'SmartNFClassifier';
+  showCamera = false;
+  config: ScannerQRCodeConfig = {
+    constraints: {
+      video: {
+        facingMode: 'environment',
+      },
+    },
+  };
+
+  openCamera() {
+    this.showCamera = true;
+    setTimeout(() => {
+      //@ts-ignore
+      document.querySelector('.ngx-scanner-qrcode').style.display = 'flex';
+      this.action.start();
+    }, 100);
+  }
+
+  readQR(event: ScannerQRCodeResult[], action?: any) {
+    console.log(event[0].value);
+    alert("Nota escaneada!")
+    this.action.stop();
+    //@ts-ignore
+    document.querySelector('.ngx-scanner-qrcode').style.display = 'none';
+  }
 }
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LOAD_WASM, NgxScannerQrcodeModule } from 'ngx-scanner-qrcode';
+
+LOAD_WASM().subscribe((res: any) => console.log('LOAD_WASM', res));
+
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NgxScannerQrcodeModule],
   providers: [],
   bootstrap: [AppComponent],
 })
